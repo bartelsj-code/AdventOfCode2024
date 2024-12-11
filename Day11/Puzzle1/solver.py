6b4cc8335df1173a89b3aa852032af0f3e7f8ca8
jobro <jonasbartels8@gmail.com>
2024-12-11 15:25:37 -0600

    Day 11 puzzle 1

---
+++ Day11/Puzzle1/solver.py
@@ -0,0 +1,39 @@
+
+def convert_input():
+    f = open("Day11\input.txt", "r")
+    output=[]
+    for b in f.readlines():
+        output = [int(gag) for gag in b.strip().split(" ")]
+        
+    f.close()
+    return output
+
+def get_next(item):
+    if item == 0:
+        return [1]
+    strv = str(item)
+    if len(strv)%2 == 0:
+
+        str1 = strv[:len(strv)//2]
+        str2 = strv[len(strv)//2:]
+        return [int(str1), int(str2)]
+    return [item * 2024]
+
+
+
+def main():
+    
+    input = convert_input()
+    current = input
+    for i in range(25):
+
+        lst = []
+        for item in current:
+
+            updated = get_next(item)
+
+            lst += updated
+        current = lst
+        
+    print(len(current))
+main()
