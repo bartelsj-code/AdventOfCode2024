37e22f80ac591f8bacf978ef8edc9e2856e5d589
jobro <jonasbartels8@gmail.com>
2024-12-19 16:31:09 -0800

    day 19 p 2

--- Day19/Puzzle2/solver.py
+++ Day19/Puzzle2/solver.py
@@ -14,32 +14,26 @@ def convert_input():
    
     return towels, out
 
-
-
 def main():
     towels, sequences = convert_input()
     towels = set(towels)
     total = 0
     for sequence in sequences:
-        mem_lst = ["."]*(len(sequence)+1)
-        mem_lst[0] = True
+        mem_lst = [0]*(len(sequence)+1)
+        mem_lst[0] = 1
         for i in range(len(sequence)):
             
             for j in range(i, i-9, -1):
                 if j < 0:
                     break
                 if sequence[j:i+1] in towels:
-                    if mem_lst[j]:
-                        mem_lst[i+1] = True
-                        break
+                    if mem_lst[j] != 0:
+                        mem_lst[i+1] += mem_lst[j]
 
-            if mem_lst[i+1] != True:
-                mem_lst[i+1] = False
+        total += mem_lst[-1]
 
-        if mem_lst[-1] == True:
-            total += 1
     print(total)
- 
+
 
             
             
