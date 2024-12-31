f7cef5df44c34ccd1d4290f4bfc29e045073b4bf
jobro <jonasbartels8@gmail.com>
2024-12-31 03:45:42 -0800

    Day 24 p 2 solved by using code to extract data and infer answers rather than having code spit out solution

--- Day24/Puzzle2/solver.py
+++ Day24/Puzzle2/solver.py
@@ -1,10 +1,10 @@
 from wire import Wire
 from gate import AndGate, OrGate, XorGate
 from machine import Machine
-
+from collections import deque
 
 def convert_input():
-    f = open("Day24\input.txt", "r")
+    f = open("Day24\Puzzle2\input_with_changes.txt", "r")
     lines = []
     for b in f.readlines():
         lines.append(b.strip())
@@ -56,9 +56,8 @@ def main():
 
     m1 = Machine(gates, wires)
 
-
     ##################################################################
-    ############ Correct Implementation to Compare With  #############
+    ############  Correct Implementation to Compare With  ############
     ##################################################################
 
     c_wires = {}
@@ -106,18 +105,17 @@ def main():
         c_gates += [xor1, xor2, and1, and2, or1]
         carry = or_out
 
-    
-
     for gate in c_gates:
         gate.assign_relations()
 
     m2 = Machine(c_gates, c_wires)
-    # print(m2.gates)
+
+    #################################  Compare  ##################################
 
     m1_known_matches = m1.val1_bits+m1.val2_bits+m1.out_bits
     m2_known_matches = m2.val1_bits+m2.val2_bits+m2.out_bits
 
-    wire_translation_for={m1_known_matches[i].name:m2_known_matches[i].name for i in range(len(m1_known_matches))}
+    wire_translation_for  = {m1_known_matches[i].name:m2_known_matches[i].name for i in range(len(m1_known_matches))}
     wire_translation_back = {m2_known_matches[i].name:m1_known_matches[i].name for i in range(len(m1_known_matches))}
 
     m2_gates = {}
@@ -127,67 +125,74 @@ def main():
 
 
     one_identified = set()
-    two_identified = set()
+    two_identified = deque()
     for bit in m1.val1_bits+m1.val2_bits:
         name = bit.name
         for gate in bit.pullers:
             if gate in one_identified:
-                two_identified.add(gate)
+                two_identified.append(gate)
                 one_identified.remove(gate)
             else:
                 one_identified.add(gate)
 
     while len(two_identified) > 0:
-        gate = two_identified.pop()
-        print(gate)
-        names = [wire_translation_for[g.name] for g in gate.inputs]
 
+        gate = two_identified.popleft()
+        names = [wire_translation_for[g.name] for g in gate.inputs]
         in_wires1 = (names[0], gate.tag, names[1])
- 
         m2_gate = None
         if in_wires1 in m2_gates:
             m2_gate = m2_gates[in_wires1]
-            print(f"\t{m2_gate}")
+            print(f"found gate matching {gate}: {m2_gate}")
 
-        if m2_gate == None or type(m2_gate) != type(gate):
-            print(gate)
-            print(wire_translation_for)
-            # print(f"{in_wires1} not found or wrong type in template. originally:{gate}")
-            # print("problem noticed")
+        if m2_gate == None:
+            print(f"\n\t match not found in template. searching match for {gate}, would have been {in_wires1}")
 
-            # top_gates = [wire.pusher for wire in gate.inputs]
-            # for top_gate in top_gates:
-            #     names = [wire_translation_for[g.name] for g in top_gate.inputs]
-            #     tup = (names[0], top_gate.tag, names[1])
+            m2_parents= []
+            for upper in [i.pusher for i in gate.inputs]:
+                m2_parents.append(m2_gates[(wire_translation_for[upper.inputs[0].name],
+                                    upper.tag,
+                                    wire_translation_for[upper.inputs[1].name])])
 
-            #     c_top = m2_gates[tup]
-            #     print(f"correct top: \t\t\t {c_top}")
-
-            #     correct_children      
-            #     print(c_top.output.pullers)
-            #     # print(m2)
+            for m2p in m2_parents:
+                print(m2p)
+                print("\t", m2p.output.pullers)
                 
-            #     # possible_positions = 
 
-            #     # print(in_wires1)
-                
+                # print(m2_gates[(q.inputs[1].name, q.tag, q.inputs[0].name)])
 
             break
-            continue
+            
+        if gate.output.name in wire_translation_for and wire_translation_for[gate.output.name] != m2_gate.output.name:
+            print("dictionary_rewrite:")
+            print(f"\t{wire_translation_for[gate.output.name]} where it should be {m2_gate.output.name:}")
 
-            candidate_pairings = []
-            
-            
-        if gate.output.name not in wire_translation_for and m2_gate.output.name not in wire_translation_back:
+        elif m2_gate.output.name in wire_translation_back and wire_translation_back[m2_gate.output.name] != gate.output.name:
+            print("dictionary_rewrite2:")
+            print(f"\t{gate.output.name} where it should be {wire_translation_back[m2_gate.output.name]}")
+
+        else:
             wire_translation_for[gate.output.name] = m2_gate.output.name
             wire_translation_back[m2_gate.output.name] = gate.output.name
 
         for g in gate.output.pullers:
             if g in one_identified:
-                two_identified.add(g)
+                two_identified.append(g)
                 one_identified.remove(g)
             else:
                 one_identified.add(g)    
+            
+
+    print(wire_translation_for)
+
+
+    ################### Not a clean solve, but done without outside help ##################
+
+    ans = "z35,sgj,z14,vss,kdh,hjf,z31,kpp".split(",")
+    ans.sort()
+    ans = ",".join(ans)
+    print(ans)
+
 
 main()
 
